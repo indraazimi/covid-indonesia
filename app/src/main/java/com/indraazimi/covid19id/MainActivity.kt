dccebb56b8968f7aa548aabcf056549c5e32a607
Indra Azimi <indraazimi@yahoo.com>
2020-09-08 12:10:45 +0700

    Melakukan request data ke API dengan Retrofit

--- app/src/main/java/com/indraazimi/covid19id/MainActivity.kt
+++ app/src/main/java/com/indraazimi/covid19id/MainActivity.kt
@@ -11,11 +11,14 @@ package com.indraazimi.covid19id
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
