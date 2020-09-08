3e7f6200d364a8509b4f9290c77db5849653c216
Indra Azimi <indraazimi@yahoo.com>
2020-09-08 15:52:24 +0700

    Menambahkan grafik dengan MPAndroidChart

--- app/src/main/java/com/indraazimi/covid19id/MainActivity.kt
+++ app/src/main/java/com/indraazimi/covid19id/MainActivity.kt
@@ -9,13 +9,19 @@
 
 package com.indraazimi.covid19id
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,13 +30,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        with(chart) {
+            setNoDataText(getString(R.string.belum_ada_data))
+            description.text = ""
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            axisLeft.axisMinimum = 0f
+            axisRight.isEnabled = false
+
+            legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+            legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+            legend.setDrawInside(false)
+        }
+
         val adapter = MainAdapter()
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.getData().observe(this, Observer { adapter.setData(it) })
+        viewModel.getEntries().observe(this, Observer { updateChart(it) })
         viewModel.getStatus().observe(this, Observer { updateProgress(it) })
+    }
+
+    private fun updateChart(entries: List<Entry>) {
+        val dataset = LineDataSet(entries, getString(R.string.jumlah_kasus_positif))
+        dataset.color = ContextCompat.getColor(this, R.color.colorPrimary)
+        dataset.fillColor = dataset.color
+        dataset.setDrawFilled(true)
+        dataset.setDrawCircles(false)
+
+        chart.data = LineData(dataset)
+        chart.invalidate()
     }
 
     private fun updateProgress(status: ApiStatus) {
