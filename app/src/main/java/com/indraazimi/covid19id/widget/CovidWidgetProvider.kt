4476289a34969915d97efe0174384f1de38f4945
Indra Azimi <indraazimi@yahoo.com>
2020-12-05 15:22:28 +0700

    Membuat widget dapat diklik

--- app/src/main/java/com/indraazimi/covid19id/widget/CovidWidgetProvider.kt
+++ app/src/main/java/com/indraazimi/covid19id/widget/CovidWidgetProvider.kt
@@ -9,13 +9,34 @@
 
 package com.indraazimi.covid19id.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.indraazimi.covid19id.MainActivity
+import com.indraazimi.covid19id.R
 
 class CovidWidgetProvider : AppWidgetProvider() {
 
-    override fun onUpdate(context: Context?, manager: AppWidgetManager?, ids: IntArray?) {
+    companion object {
+        private fun updateAppWidget(context: Context, manager: AppWidgetManager, id: Int) {
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            val views = RemoteViews(context.packageName, R.layout.widget_main)
+            views.setOnClickPendingIntent(R.id.dataTextView, pendingIntent)
+            manager.updateAppWidget(id, views)
+        }
+    }
 
+    override fun onUpdate(context: Context?, manager: AppWidgetManager?, ids: IntArray?) {
+        if (context == null || manager == null || ids == null) return
+
+        // Pengguna dapat menambahkan lebih dari 1 widget di home screen
+        // Jadi kita harus melakukan update ke semua widget yang ada.
+        for (id in ids) {
+            updateAppWidget(context, manager, id)
+        }
     }
 }
