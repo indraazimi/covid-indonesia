261f47067d1b0d9b374a2168e25c4807c2fe1257
Indra Azimi <indraazimi@yahoo.com>
2020-12-05 10:02:59 +0700

    Membuat widget dengan tampilan statis

---
+++ app/src/main/java/com/indraazimi/covid19id/widget/CovidWidgetProvider.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.covid19id.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class CovidWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context?, manager: AppWidgetManager?, ids: IntArray?) {
+
+    }
+}
