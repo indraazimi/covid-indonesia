906a50c9294f6746cbcb1939b1911b4fc6ede654
Indra Azimi <indraazimi@yahoo.com>
2020-12-06 08:40:20 +0700

    Update tampilan widget sesuai data

--- app/src/main/java/com/indraazimi/covid19id/widget/CovidWidgetProvider.kt
+++ app/src/main/java/com/indraazimi/covid19id/widget/CovidWidgetProvider.kt
@@ -15,8 +15,11 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import androidx.preference.PreferenceManager
 import com.indraazimi.covid19id.MainActivity
 import com.indraazimi.covid19id.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CovidWidgetProvider : AppWidgetProvider() {
 
@@ -25,8 +28,28 @@ class CovidWidgetProvider : AppWidgetProvider() {
             val intent = Intent(context, MainActivity::class.java)
             val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
             val views = RemoteViews(context.packageName, R.layout.widget_main)
-            views.setOnClickPendingIntent(R.id.dataTextView, pendingIntent)
+            views.setOnClickPendingIntent(R.id.dataPanel, pendingIntent)
+
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val date = prefs.getLong(WidgetUtils.KEY_DATE, -1L)
+            val data = prefs.getInt(WidgetUtils.KEY_DATA, -1)
+            updateUI(context, views, date, data)
+
             manager.updateAppWidget(id, views)
+        }
+
+        private fun updateUI(context: Context, views: RemoteViews, date: Long, data: Int) {
+            if (date == -1L || data == -1) {
+                views.setTextViewText(R.id.dateTextView, context.getString(R.string.belum_ada_data))
+                views.setTextViewText(R.id.dataTextView, context.getString(R.string.sad_emoji))
+                views.setTextViewText(R.id.descTextView, context.getString(R.string.klik_refresh))
+            }
+            else {
+                val formatter = SimpleDateFormat("dd MMMM yyyy", Locale("ID", "id"))
+                views.setTextViewText(R.id.dateTextView, formatter.format(date))
+                views.setTextViewText(R.id.dataTextView, data.toString())
+                views.setTextViewText(R.id.descTextView, context.getString(R.string.kasus_positif))
+            }
         }
     }
 
