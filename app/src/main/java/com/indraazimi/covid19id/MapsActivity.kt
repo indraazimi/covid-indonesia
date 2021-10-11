6d0a5dc11facec92746f1c6abeaa64682c6f6994
Indra Azimi <indraazimi@yahoo.com>
2021-10-11 21:59:45 +0700

    Menampilkan data dari API ke peta

--- app/src/main/java/com/indraazimi/covid19id/MapsActivity.kt
+++ app/src/main/java/com/indraazimi/covid19id/MapsActivity.kt
@@ -12,15 +12,21 @@ package com.indraazimi.covid19id
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.indraazimi.covid19id.model.Provinsi
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val viewModel: MapsViewModel by lazy {
         ViewModelProvider(this).get(MapsViewModel::class.java)
     }
+
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +35,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        viewModel.getData().observe(this, { data ->
+            data.forEach { createMarker(it) }
+        })
+    }
+
+    private fun createMarker(prov: Provinsi) {
+        val lokasi = LatLng(prov.lokasi.lat, prov.lokasi.lon)
+        val snippet = getString(R.string.jumlah_dirawat, prov.dirawat)
+        map.addMarker(MarkerOptions()
+            .position(lokasi)
+            .title(prov.key)
+            .snippet(snippet))
     }
 
     override fun onMapReady(gMap: GoogleMap) {
+        map = gMap
         viewModel.requestData()
+
+        val lokasi = LatLng(-6.920432082789247, 107.60370834146391)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(lokasi, 7f))
     }
 }
--- app/src/main/java/com/indraazimi/covid19id/MapsViewModel.kt
+++ app/src/main/java/com/indraazimi/covid19id/MapsViewModel.kt
@@ -10,22 +10,29 @@
 package com.indraazimi.covid19id
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.indraazimi.covid19id.model.Provinsi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MapsViewModel : ViewModel() {
 
+    private val data = MutableLiveData<List<Provinsi>>()
+
     fun requestData() = viewModelScope.launch(Dispatchers.IO) {
         withContext(Dispatchers.IO) {
             try {
                 val result = Covid19Api.service.getProvinsi()
-                Log.d("MAPS", "Jumlah data: " + result.data.size)
+                data.postValue(result.data)
             } catch (e: Exception) {
                 Log.d("MAPS", e.message.toString())
             }
         }
     }
+
+    fun getData(): LiveData<List<Provinsi>> = data
 }
